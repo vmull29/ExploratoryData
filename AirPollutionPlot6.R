7ef83588d5d153a2f4e10abfd8dfa21d7e6e3d28
vmull29 <56950760+vmull29@users.noreply.github.com>
2020-08-09 13:39:35 -0700

    Add files via upload

---
+++ AirPollutionPlot6.R
@@ -0,0 +1,9 @@
+> baltLosAngelesMotors <- subset(NEI, NEI$fips %in% c("24510","06037") & NEI$type == "ON-ROAD")
+> baltLosAngelesMotorsAGG <- aggregate(Emissions ~ year + fips, baltLosAngelesMotors, sum)
+> ggplot(baltLosAngelesMotorsAGG, aes(year, Emissions, col = fips)) +
++       geom_line() +
++       geom_point() +
++       ggtitle(expression("Baltimore and Los Angeles" ~ PM[2.5] ~ "Motor Vehicle Emissions by Year")) +
++       labs(x = "Year", y = expression(~PM[2.5]~ "Motor Vehicle Emissions") ) +
++       scale_colour_discrete(name = "City", labels = c("Los Angeles", "Baltimore")) +
++       theme(legend.title = element_text(face = "bold"))
